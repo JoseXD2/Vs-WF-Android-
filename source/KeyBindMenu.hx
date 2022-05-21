7eb2f3707bcdc151dc7a823795fcfd7f18e0b1ed
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-05-21 13:43:19 -0300

    É isso?

--- source/KeyBindMenu.hx
+++ source/KeyBindMenu.hx
@@ -12,6 +12,7 @@ import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
+import flixel.FlxSubState;
 #if newgrounds
 import io.newgrounds.NG;
 #end
