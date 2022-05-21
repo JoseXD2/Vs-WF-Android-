3a1a3281f0c20bcc0ffefd8bb24835344292affc
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-05-21 18:53:49 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -91,7 +91,7 @@ class PlayState extends MusicBeatState
 	public static var rep:Replay;
 	public static var loadRep:Bool = false;
         public var addhitbox:FlxSpriteGroup;
-	public var addFlxHitbox
+	public var addFlxHitbox;
 	
 	
 	public static var noteBools:Array<Bool> = [false, false, false, false];
