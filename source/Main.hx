87e623fe60f777cad9e9bbe5316cc1a7db87f295
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-05-21 19:12:17 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -39,8 +39,6 @@ class Main extends Sprite
 	{
 		super();
 
- 	        SUtil.gameCrashCheck();
-
 		if (stage != null)
 		{
 			init();
@@ -74,8 +72,6 @@ class Main extends Sprite
 			gameWidth = Math.ceil(stageWidth / zoom);
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
-
-		SUtil.doTheCheck();
 
 		#if !debug
 	//	initialState = WarningState; //CHANGE THIS TO BIRDY MENU LOL
